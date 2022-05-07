70b6e534ba5f7ee13ef249cec6ca489ef25ca989
sarcy  nenepat <82246009+sarcy29@users.noreply.github.com>
2022-05-07 11:40:41 +0900

    adding index.php

---
+++ New folder/dbh_inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$adServename = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($adServename, $dbUsername, $dbPassword, $dbName);
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include_once 'includes/dbh_inc.php';
+
+?>
+
+<!DOCTYPE html>
+<head>
+    <title>How to connect to a database in PHP</title>
+</head>
+<body>
+    
+</body>
+</html>
